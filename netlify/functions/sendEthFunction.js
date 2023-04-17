77929fcef7bc977716b3ee55ab84bd74928c4ccb
santiagosimulated <santiagosimulated@gmail.com>
2023-04-17 08:55:48 -0700

    console

--- netlify/functions/sendEthFunction.js
+++ netlify/functions/sendEthFunction.js
@@ -3,6 +3,8 @@ const sendEth = require('../../sendEth');
 
 exports.handler = async (event, context) => {
   sendEth();
+
+  console.log("triggered");
   return {
     statusCode: 200,
     body: 'Send ETH function executed'
