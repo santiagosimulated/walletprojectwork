8f32eae966e890c8f1c7a699bff959b92e6cc113
santiagosimulated <santiagosimulated@gmail.com>
2023-04-17 10:29:26 -0700

    console

--- netlify/functions/sendEthFunction.js
+++ netlify/functions/sendEthFunction.js
@@ -1,4 +1,4 @@
-const sendEth = require('../../sendEth.js');
+const sendEth = require('../sendEth.js');
 
 module.exports = async () => {
   console.log('Running sendEth function on server startup...');
